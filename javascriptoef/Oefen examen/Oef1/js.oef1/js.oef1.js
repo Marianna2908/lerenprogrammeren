cffebc096d47267d3a02f83f38d359103c59a7f3
Marianna2908 <mar-ivanov94@hotmail.com>
2021-09-25 11:43:54 +0200

    lerenprogrammeren

--- javascriptoef/Oefen examen/Oef1/js.oef1/js.oef1.js
+++ javascriptoef/Oefen examen/Oef1/js.oef1/js.oef1.js
@@ -29,14 +29,14 @@
 
 //3. Schrijf een programma waar je het eindGetal vraag aan de gebruiker en als volgt afdruk op het scherm.
 //
-// var eindGetal = "";
+// var eindgetal = "";
 
 // var totaal= 2;
 //
 //
 // begingetal= parseInt(prompt("geef het getal in"));
 // for(i=1;i<=eindgetal;i++){
-//     if
+//     if(
 //
 // console.log(teller + "+"+ getal +"=" +som)
 // }
@@ -64,3 +64,94 @@
 // console.log("Aantal woorden: " + aantalWoorden);
 //
 //
+/*
+// /*vraag aan de gebruiker zijn naam welk beroep hij heeft, bediende, arbeider, of werkzoekende*/
+// var naam = prompt("geef je naam in ");
+// var beroep= prompt("geef je beroep in, maak een keuze uit arbeider, bediende of werkzoekende");
+//
+// if(beroep =="bediende"){
+//     console.log("beroep van "+ naam + " is " + beroep);
+// }else if(beroep == "arbeider"){
+//         console.log("Beroep van "+ naam + " is " + beroep);
+// }else{
+//     console.log("beroep van " + naam + " is "+ beroep);
+// }
+// */
+//
+// for ( var i= 10; i>0; i--){
+//     console.log(i);
+// }
+// for(var i= 1; i<=10;i+=2){
+//     console.log(i);
+// }
+// for(var i = 1; i<=10; i--){
+//     console.log(i);
+// }
+
+// var text= " ";
+// var i = 0
+//
+// while(i<10){
+//     text += "de nummers zijn "+ i;
+//     i++;
+// }
+// console.log(text);
+//
+// var elements = document.getElementsByClassName("*");
+// console.time(" loop time");
+// for ( var i=0; i<5000; i++){
+//     for(var j=0, length= elements.length; j <length; j++){
+//         //nothing to do
+//     }
+// }
+// console.timeEnd("Loop time");
+
+// var leeftijd = parseInt(prompt(" wat is je leeftijd"));
+// if(leeftijd>= 18){
+//     console.log(" je bent " + leeftijd + "jaar oud, je mag meedoen");
+// }else{(leeftijd<18)
+//     console.log(" je leeftijd is " + leeftijd + " minimumleeftijd om deel te nemen aan de loterij is 18j")
+// }
+// var geboorteJaar = parseInt(prompt(" Geef je geboortejaar in"));
+// var lopendJaar= parseInt((prompt(" Geef het huidig jaartal in")));
+//
+// if(lopendJaar- geboorteJaar>= 18){
+//     console.log( "vanaf nu mag ik, kan en beslis ik alles binnen de wettelijke grenzen")
+// }else{
+//     console.log(" Gelukkig heb ik mijn ouders die alles voor me regelen ")
+// }
+//
+
+// getal1 = parseInt((prompt("Geef getal 1 in")));
+// getal2 = parseInt((prompt("Geef getal 2 in")));
+// getal3 = parseInt((prompt("Geef getal 3 in")));
+// som = "";
+// var teller = 1;
+//
+// for(i= teller; i<=teller ; i++){
+//     som = getal1+ getal2+ getal3;
+//     i++
+// }
+// console.log(" de som van de 3 getallen is " + som)
+
+// var aantalSterren = parseInt(prompt(" geef het aantal sterretjes in"));
+// var sterretjes = "*";
+// var totaal = sterretjes;
+// var resultaat = "";
+//
+// for(i=1; i<=aantalSterren;i++){
+//    resultaat = resultaat+sterretjes;
+//    console.log(resultaat);
+// }
+
+// var aantalSterren= parseInt(prompt("geef het maximumaantal sterren in"));
+// var sterretjes = "*";
+// var resultaat = " ";
+//
+// for(var i=1; i>= aantalSterren;--i){;
+//     sterretjes-= "*";
+//     console.log(sterretjes);
+// }
+//
+//
+
